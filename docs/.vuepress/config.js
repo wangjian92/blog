c3f9a3db48261c94d0bef684d35647d01a1c9c17
wj <342153912@qq.com>
2019-10-21 07:40:08 +0800

    :tada: init blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+const themeConfig = require('./config/theme/')
+
+module.exports = {
+  title: "恍然大明白",
+  description: 'Take your passion and make it come true.',
+  dest: 'public',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  theme: 'reco',
+  themeConfig,
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: ['@vuepress/medium-zoom', 'flowchart'] 
+}  
---
+++ docs/.vuepress/config/nav/index.js
@@ -0,0 +1,12 @@
+module.exports = [
+  { text: 'Home', link: '/', icon: 'reco-home' },
+  { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+  { text: 'Contact', 
+    icon: 'reco-message',
+    items: [
+      { text: 'NPM', link: 'https://www.npmjs.com/~wangjian92', icon: 'reco-npm' },
+      { text: 'GitHub', link: 'https://github.com/wangjian92', icon: 'reco-github' },
+      { text: '简书', link: 'https://www.jianshu.com/u/375fdabb8c4f', icon: 'reco-jianshu' }
+    ]
+  }
+]

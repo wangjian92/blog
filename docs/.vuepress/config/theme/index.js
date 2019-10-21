6d58a24dfb70ddce0ce178313ade158cadbfa088
wj <342153912@qq.com>
2019-10-21 15:38:20 +0800

    style

--- docs/.vuepress/config/theme/index.js
+++ docs/.vuepress/config/theme/index.js
@@ -2,13 +2,23 @@ const themeReco = require('./themeReco.js')
 const nav = require('../nav/')
 const sidebar = require('../sidebar/')
 
-module.exports = Object.assign({}, themeReco, {
-  nav,
-  sidebar,
-  // logo: '/head.png',
-  // 搜索设置
-  search: true,
-  searchMaxSuggestions: 10,
-  // 自动形成侧边导航
-  sidebar: 'auto',
-})
+module.exports = Object.assign(
+  {},
+  themeReco,
+  {
+    nav,
+    sidebar,
+    // logo: '/head.png',
+    // 搜索设置
+    search: true,
+    searchMaxSuggestions: 10,
+    // 自动形成侧边导航
+    sidebar: 'auto',
+    // 密钥
+    keyPage: {
+      keys: ['1221'],
+      color: '#3eaf7c', // 登录页动画球的颜色
+      lineColor: '#42b983' // 登录页动画线的颜色
+    }
+  }
+)

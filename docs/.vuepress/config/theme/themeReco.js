65998599fa2b7fb76c9945e92ac5fe9dcb2b5f2b
wj <342153912@qq.com>
2019-11-10 13:36:40 +0800

    :tada: comment

--- docs/.vuepress/config/theme/themeReco.js
+++ docs/.vuepress/config/theme/themeReco.js
@@ -18,5 +18,13 @@ module.exports = {
   // 备案号
   record: '',
   // 项目开始时间
-  startYear: '2017'
+  startYear: '2017',
+  // valine 设置
+  valineConfig: {
+    appId: 'Um5PchoOL8W9rVWuoGbL2p7v-MdYXbMMI',
+    appKey: 'E2SkzTUViyQgwahoF5uQwdcY',
+    placeholder: '填写邮箱可以收到回复提醒哦！',
+    notify: true,
+    recordIP: true
+  }
 }
